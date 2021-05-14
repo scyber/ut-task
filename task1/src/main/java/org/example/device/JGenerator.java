d7a05471f99c0d599bc5ebda56c58a4d6fda5e40
Григорий Пискунов <piskunov.g.nik@sberbank.ru>
2021-05-14 13:05:32 +0300

    Create builded multiproject

---
+++ task1/src/main/java/org/example/device/JGenerator.java
@@ -0,0 +1,7 @@
+package org.example.device;
+
+public class JGenerator {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ task1/src/main/scala/org/example/device/DevGenerator.scala
@@ -0,0 +1,10 @@
+package org.example.device
+
+/**
+  * This is a just json device generator example
+  */
+object DevGenerator {
+  def main(args: Array[String]): Unit = {
+    println("Hello from scala")
+  }
+}
